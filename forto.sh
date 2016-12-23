1cdb088dd04e46bf56178d040a8af5a8485cf083
Fortoszone <Fortoszone@gmail.com>
2016-12-23 14:13:53 +0700

    build script added

---
+++ forto.sh
@@ -0,0 +1,98 @@
+#
+# Copyright © 2016, Kunal Kene "kunalkene1797" <kunalkene1797@gmail.com>
+#
+# This software is licensed under the terms of the GNU General Public
+# License version 2, as published by the Free Software Foundation, and
+# may be copied, distributed, and modified under those terms.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# Please maintain this if you use this script or any part of it
+
+# Init Script
+KERNEL_DIR=$PWD
+ZIMAGE=$KERNEL_DIR/arch/arm/boot/zImage
+BUILD_START=$(date +"%s")
+
+# Color Code Script
+Black='\e[0;30m'        # Black
+Red='\e[0;31m'          # Red
+Green='\e[0;32m'        # Green
+Yellow='\e[0;33m'       # Yellow
+Blue='\e[0;34m'         # Blue
+Purple='\e[0;35m'       # Purple
+Cyan='\e[0;36m'         # Cyan
+White='\e[0;37m'        # White
+nocol='\033[0m'         # Default
+
+# Tweakable Options Below
+export ARCH=arm
+export SUBARCH=arm
+export KBUILD_BUILD_USER="Fortoszone"
+export KBUILD_BUILD_HOST="Masturb"
+export CROSS_COMPILE="/home/forto/Desktop/project/arm-eabi-4.9/bin/arm-eabi-"
+
+# (Optional) Specify a release number.
+# A "Testing" label will be used if this is left blank.
+while read -p "Do you want to specify a release/version number? (Just press enter if you don't.) " rel; do
+	if [[ `echo $rel | gawk --re-interval "/^R/"` != "" ]]; then
+		for i in $sequence; do
+			if [ `echo $rel | gawk --re-interval "/^R$i/"` ]; then
+				echo -e "Release number: $rel\n";
+				export LOCALVERSION="-FortoKernel$rel";
+				version=$rel;
+			fi;
+		done;
+	elif [[ `echo $rel | gawk --re-interval "/^v/"` ]]; then
+		echo -e "Version number: $rel\n";
+		export LOCALVERSION="-FortoKernel-$rel";
+		version=$rel;
+	else
+		case $rel in
+			"" | " " )
+				echo -e "No release number was specified. Labelling this build as testing/nightly.\n";
+				export LOCALVERSION="-FortoKernel-test";
+				version=`date --utc "+%Y%m%d.%H%M%S"`;
+				break;;
+			*)
+				echo -e "Localversion set as: $rel\n";
+				export LOCALVERSION="-FortoKernel-$rel";
+				version=$rel;
+				break;;
+		esac;
+	fi;
+	break;
+done;
+
+# Compilation Scripts Are Below
+compile_kernel ()
+{
+echo -e "$White***********************************************"
+echo "             Compiling FortoKernel             "
+echo -e "***********************************************$nocol"
+make clean && make mrproper
+make forto_falcon_defconfig
+make -j4 CONFIG_NO_ERROR_ON_MISMATCH=y
+if ! [ -a $ZIMAGE ];
+then
+echo -e "$Red Kernel Compilation failed! Fix the errors! $nocol"
+exit 1
+fi
+}
+
+# Finalizing Script Below
+case $1 in
+clean)
+make ARCH=arm -j3 clean mrproper
+rm -rf include/linux/autoconf.h
+;;
+*)
+compile_kernel
+;;
+esac
+BUILD_END=$(date +"%s")
+DIFF=$(($BUILD_END - $BUILD_START))
+echo -e "$Yellow Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
